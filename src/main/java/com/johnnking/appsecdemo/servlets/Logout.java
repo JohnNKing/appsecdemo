f0e2ed7f584f96c1f30fc52a324738d5a54c3b63
John King <jnkisd@rit.edu>
2016-06-05 00:47:16 -0400

    Logout.java missing from earlier commit (e2e4978)

---
+++ src/main/java/com/johnnking/appsecdemo/servlets/Logout.java
@@ -0,0 +1,29 @@
+package com.johnnking.appsecdemo.servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.johnnking.appsecdemo.UserManager;
+
+
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Logout() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession(true).invalidate();
+		response.sendRedirect("/appsecdemo/");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+	}
+}
