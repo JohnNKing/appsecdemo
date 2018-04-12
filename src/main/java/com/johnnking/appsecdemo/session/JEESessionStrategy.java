1b78e6f05cc21638ce281fc346a4527e8053a600
John King <jnkisd@rit.edu>
2018-04-11 23:05:16 -0400

    Preparing JEESessionStrategy for training; attendees must solve

--- src/main/java/com/johnnking/appsecdemo/session/JEESessionStrategy.java
+++ src/main/java/com/johnnking/appsecdemo/session/JEESessionStrategy.java
@@ -7,17 +7,14 @@ import javax.servlet.http.HttpSession;
 public class JEESessionStrategy implements Strategy {
 
     public String getUsername(HttpServletRequest request, HttpServletResponse response) {
-        HttpSession session = request.getSession(true);
-        String username = (String) session.getAttribute("username");
-        return username;
+        // TODO
     }
 
     public void login(String username, HttpServletRequest request, HttpServletResponse response) {
-        request.getSession(true).invalidate();
-        request.getSession(true).setAttribute("username", username);
+        // TODO
     }
 
     public void logout(HttpServletRequest request, HttpServletResponse response) {
-        request.getSession(true).invalidate();
+        // TODO
     }
 }
